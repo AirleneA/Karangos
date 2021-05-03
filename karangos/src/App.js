eb60967884605840b6b42a1f8c369e74b00ef632
AirleneA <airlene.antonelli@gmail.com>
2021-05-03 14:05:41 +0000

    03/05 continuação projeto karangos

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -1,37 +1,39 @@
-/*import logo from './logo.svg';
+/*
+import logo from './logo.svg';
 import './App.css';
-import Button from 'material-ui/core/Button'
+import Button from '@material-ui/core/Button'
+*/
+
+/*
+  <div className="App">
+    <header className="App-header">
+      <h1>Projeto Karangos</h1>
+      <Button variant="contained" color="primary">Clique aqui!</Button>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
 */
 
 
-/*   <div className="App">
-      <header className="App-header">
-        <h1>Projeto Karangos</h1>
-        <Button variant="contained"
-        color="primary">Clique aqui!</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */
 
-
-
-import { createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import yellow from '@material-ui/core/colors/yellow';
 import red from '@material-ui/core/colors/red';
-import TopBar from './ui/TopBar';
+import TopBar from './ui/TopBar'
 import FooterBar from './ui/FooterBar'
+import Box from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
   palette: {
@@ -45,13 +47,29 @@ const theme = createMuiTheme({
   },
 });
 
-    function App() {
+const useStyles = makeStyles((theme) => ({
+  main: {
+    backgroundColor: theme.palette.background.default,
+    minHeight: '100vh', // 100% da altura da área de visualização
+    margin: '0 0 42px 0'  // margem de 42px inferior para acomodar a barra inferior
+  }
+}))
+
+function Main() {
+  const classes = useStyles()
+  return (
+    <Box className={classes.main}>
+      <TopBar />
+      <FooterBar />
+    </Box>
+  )
+}
+
+function App() {
   return (
     <ThemeProvider theme={theme}>
-    <TopBar/>
-    <FooterBar/>
+      <Main />
     </ThemeProvider>
- 
   );
 }
 
