6465971f862e60f05d49c802882c3400dbc89303
AirleneA <airlene.antonelli@gmail.com>
2021-05-11 21:23:38 +0000

    correção aula 10/05 inicio do componente de listagem

--- karangos/src/routed/KarangosList.js
+++ karangos/src/routed/KarangosList.js
@@ -1,81 +1,79 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { DataGrid, useGridParamsApi } from '@material-ui/data-grid';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox'
 
-const columns = [
-  { field: 'id', headerName: 'Cód.', width: 100},
-  { field: 'marca', headerName: 'marca', width: 100},
-  { field: 'modelo', headerName: 'Modelo', width: 150},
-  { field: 'cor', headerName: 'Cor', width: 150},
-  { field: 'ano_fabricacao', headerName: 'Ano', width: 100},
-  { field: 'importado', headerName: 'Importado?', width: 125},
-  { field: 'placa', headerName: 'Placa', width: 100},
-  { 
-      field: 'preco', 
-      headerName: 'Preço', 
-      width: 150, 
-      align: 'right',
-      headerAlign: 'right',
-      valueGetter: params => {
-         // Number(params.getValue('preco').toLocaleString('pt-br'))
-        useGridParamsApi.getValue('preco')
-      }
-    },
-];
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 export default function KarangosList() {
-//Variaveis que conterão dados PRECISAM ser inicializados como vetores vazios
+    const classes = useStyles()
+  
+    // Variáveis que conterão dados PRECISAM ser inicializadas como vetores vazios
     const [karangos, setKarangos] = useState([])
-
+  
     useEffect(() => {
-        async function getData() {
-            try {// tenta buscar os dados
-                let data = await axios.get('https://api.faustocintra.com.br/karangos')
-                setKarangos(data)
-            }
-            catch(error) {
-                console.error(error)
-            }
+      async function getData() {
+        try { // tenta buscar os dados
+          let response = await axios.get('https://api.faustocintra.com.br/karangos')
+          setKarangos(response.data)
         }
-        getData()
-    },[])// Quando a lista de dependencia é um vetor vazio, o useEffect()
-    // é executado apenas uma vez, no carregamento inicial do componente
-
+        catch(error) {
+          console.error(error)
+        }
+      }
+      getData()
+    }, []) // Quando a lista de dependências é um vetor vazio, o useEffect()
+           // é executado apenas uma vez, no carregamento inicial do componente
+  
     return (
         <>
-            <h1>Listagem de Karangos</h1>
-                <TableContainer component={Paper}>
-                <Table className={classes.table} size="small" aria-label="a dense table">
-                <TableHead>
-                <TableRow>
-                <TableCell>Cód.</TableCell>
-                <TableCell>Marca</TableCell>
-                <TableCell>Modelo</TableCell>
-                <TableCell>Cor</TableCell>
-                <TableCell>Ano</TableCell>
-                <TableCell>Importado?</TableCell>
-                <TableCell>Placa</TableCell>
-                <TableCell align="right">Preço</TableCell>
-             </TableRow>
-            </TableHead>
-        <TableBody>
-        {karangos.map((karango) => (
-                <TableRow key={karango.id}>
-        <TableCell>{karango.id}</TableCell>
-        <TableCell>{karango.marca}</TableCell>
-        <TableCell>{karango.modelo}</TableCell>
-        <TableCell>{karango.cor}</TableCell>
-        <TableCell>{karango.ano_fabricacao}</TableCell>
-        <TableCell>{karango.importado}</TableCell>
-        <TableCell>{karango.placa}</TableCell>
-        <TableCell align="right">
-            {karango.preco
-                        }</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-        )
-}
+        <h1>Listagem de Karangos</h1>
+        <TableContainer component={Paper}>
+        <Table className={classes.table} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Cód.</TableCell>
+              <TableCell>Marca</TableCell>
+              <TableCell>Modelo</TableCell>
+              <TableCell>Cor</TableCell>
+              <TableCell>Ano</TableCell>
+              <TableCell align="center">Importado?</TableCell>
+              <TableCell>Placa</TableCell>
+              <TableCell align="right">Preço</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {karangos.map((karango) => (
+              <TableRow key={karango.id}>
+                <TableCell>{karango.id}</TableCell>
+                <TableCell>{karango.marca}</TableCell>
+                <TableCell>{karango.modelo}</TableCell>
+                <TableCell>{karango.cor}</TableCell>
+                <TableCell>{karango.ano_fabricacao}</TableCell>
+                <TableCell align="center">
+                <Checkbox checked={karango.importado === 1} readonly="readonly" />
+                </TableCell>
+                <TableCell>{karango.placa}</TableCell>
+                <TableCell align="right">
+                  { Number(karango.preco).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }
+                </TableCell>
+              </TableRow>
+            ))}
+            </TableBody>
+          </Table>
+        </TableContainer>      
+      </>
+    )
+  } 
