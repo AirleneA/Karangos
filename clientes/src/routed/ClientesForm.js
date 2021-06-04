dabe73167ca82d415f5259f6992864f2dacf027e
AirleneA <airlene.antonelli@gmail.com>
2021-06-04 18:01:41 +0000

    trabalho

--- clientes/src/routed/ClientesForm.js
+++ clientes/src/routed/ClientesForm.js
@@ -3,15 +3,18 @@ import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
 import FormControl from '@material-ui/core/FormControl'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 import InputMask from 'react-input-mask'
 import { makeStyles } from '@material-ui/core/styles'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
 import axios from 'axios'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 import Snackbar from '@material-ui/core/Snackbar'
 import MuiAlert from '@material-ui/lab/Alert'
+import React from 'react'
+import ConfirmDialog from '../ui/ConfirmDialog'
 
 const useStyles = makeStyles(theme => ({
   form: {
@@ -34,28 +37,38 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export default function ClientesForm() {
+export default function ClinetesForm() {
   const classes = useStyles()
 
   const estados = [
-    'Amarelo',
-    'Azul',
-    'Bege',
-    'Branco',
-    'Cinza',
-    'Dourado',
-    'Laranja',
-    'Marrom',
-    'Prata',
-    'Preto',
-    'Rosa',
-    'Roxo',
-    'Verde',
-    'Vermelho',
-    'Vinho'
-  ]
-
-   // Classes de caracters para a máscara da placa
+    `AL`,
+    `AP`,
+    `AM`,
+    `BA`,
+    `CE`,
+    `DF`,
+    `ES`,
+    `GO`,
+    `MA`,
+    `MT`,
+    `MS`,
+    `MG`,
+    `PA`,
+    `PB`,
+    `PR`,
+    `PE`,
+    `PI`,
+    `RJ`,
+    `RN`,
+    `RS`,
+    `RO`,
+    `RR`,
+    `SC`,
+    `SP`, 
+    `SE`,
+    `TO`, 
+]
+  // Classes de caracters para a máscara da placa
   // 1) Três primeiras posições, somente letras (maiúsculas ou minúsculas) ~> [A-Za-z]
   // 2) Quinta, sétima e oitava posições, somente dígitos ~> [0-9]
   // 3) Sexta posição: dígitos ou letras (maiúsculas ou minúsculas) de A a J ~> [0-9A-Ja-j]
@@ -65,14 +78,8 @@ export default function ClientesForm() {
     '#': '[0-9A-Ja-j]'
   }
 
-  
-  // Máscara de entrada para a placa
-  const cpfMask = '000.000.000-00'
-
-  const ufMask = 'AA'
-
   // Máscara para CPF: '000.000.000-00'
-  // Máscara para CNPJ: '00.000.000/0000-00'
+  const cpfMask = '000.000.000-00'
 
   const [cliente, setCliente] = useState({
     id: null,
@@ -80,15 +87,14 @@ export default function ClientesForm() {
     cpf: '',
     rg: '',
     logradouro: '',
-    num_imovel: '0',
-    complemento: '',
+    num_imovel: '',
+    complemento:'',
     bairro: '',
-    municipio: '',
-    uf: '',
-    telefone: '0',
+    município: '',
+    uf: '', 
+    telefone: '',
     email: '',
   })
-  
   const [currentId, setCurrentId] = useState()
 
   const [sendBtnStatus, setSendBtnStatus] = useState({
@@ -101,47 +107,160 @@ export default function ClientesForm() {
     severity: 'success',
     message: '' 
   })
+
   const [error, setError] = useState({
-    marca: null,
-    modelo: null,
-    placa: null,
-    preco: null
+    marca: '',
+    modelo: '',
+    cor: '',
+    placa: '',
+    preco: ''
   })
 
-  const [isValid, setIsValid ] = useState(false)
+  const [isModified, setIsModified] = useState(false)
 
-  const [isValid, setIsValid ] = useState(false)
+  const [dialogOpen, setDialogOpen] = useState(false) // O diálogo de confirmação está aberto?
+
+  const [title, setTitle] = useState('Cadastrar novo karango')
 
   const history = useHistory()
+  const params = useParams()
+
+  // useEffect() para quando o formulário for carregado (só na inicialização)
+  useEffect(() => {
+    // Verificamos se a rota atual contém o parâmetro id
+    // Em caso positivo, buscamos os dados no back-end e carregamos o formulário para edição
+    if(params.id) {
+      setTitle('Editar cliente')
+      getData(params.id)
+    }
+  }, [])
+
+  async function getData(id) {
+    try {
+      let response = await axios.get(`https://api.faustocintra.com.br/clientes/${id}`)
+      setCliente(response.data)
+    }
+    catch(error) {
+      setSbStatus({
+        open: true,
+        severity: 'error',
+        message: 'Não foi possível carregar os dados para edição.'
+      })
+    }
+  }
 
   function handleInputChange(event, property) {
+
+    const clienteTemp = {...cliente}
+
     setCurrentId(event.target.id)
     if(event.target.id) property = event.target.id
-    else if(property === 'cpf') {
-      setCliente({...cliente, cpf: event.target.value.toUpperCase()})
-    }
-    else if(property === 'uf') {
-      setCliente({...cliente, uf: event.target.value.toUpperCase()})
-    }
 
+   if(property === 'cpf') {
+      clienteTemp.cpf = event.target.value.toUpperCase()
+    }
     else {
       // Quando o nome de uma propriedade de objeto aparece entre [],
       // significa que o nome da propriedade será determinado pela
       // variável ou expressão contida dentro dos colchetes
-      setCliente({...cliente, [property]: event.target.value})
+     clienteTemp[property] = event.target.value
     }
+    setCliente(clienteTemp)
+    setIsModified(true)   // O formulário foi modificado
+    validate(clienteTemp)  // Dispara a validação
+  }
+
+  function validate(data) {
+    let isValid = true
+
+    const errorTemp = {
+      nome: '',
+      cpf: '',
+      rg: '',
+      logradouro: '',
+      num_imovel: '',
+      complemento:'',
+      bairro: '',
+      município: '',
+      uf: '', 
+      telefone: '',
+      email: '',
+    }
+
+    // trim(): retira espaços em branco do início e do final de uma string
+    if(data.nome.trim() === '') {
+      errorTemp.nome = 'O nome deve ser preenchido'
+      isValid = false
+    }    
+
+    if(data.cpf.trim() === '' || data.cpf.includes('_')) {
+      errorTemp.cpf = 'O cpf deve ser preenchido corretamente'
+      isValid = false
+    }
+
+    if(data.rg.trim() === '') {
+      errorTemp.rg = 'O rg deve ser preenchido'
+      isValid = false
+    }
+    
+    if(data.logradouro.trim() === '') {
+      errorTemp.logradouro = 'O logradouro deve ser preenchido'
+      isValid = false
+    }
+
+    if(data.num_imovel.trim() === '') {
+      errorTemp.num_imovel = 'O número do imóvel deve ser preenchido'
+      isValid = false
+    }
+    if(data.bairro.trim() === '') {
+      errorTemp.bairro = 'O bairro deve ser preenchido'
+      isValid = false
+    }
+   
+    if(data.uf.trim() === '') {
+      errorTemp.cor = 'O estado deve ser informado'
+      isValid = false
+    } 
+
+    if(data.modelo.trim() === '') {
+      errorTemp.modelo = 'O modelo deve ser preenchido'
+      isValid = false
+    }
+
+    if(data.cor.trim() === '') {
+      errorTemp.cor = 'A cor deve ser informada'
+      isValid = false
+    }
+
+    // A placa não pode ser string vazia nem conter sublinhado
+    if(data.placa.trim() === '' || data.placa.includes('_')) {
+      errorTemp.placa = 'A placa deve ser preenchida corretamente'
+      isValid = false
+    }
+
+    // O preço deve ser numérico e maior que zero
+    if(isNaN(data.preco) || Number(data.preco) <= 0) {
+      errorTemp.preco = 'O preço deve ser informado e maior que zero'
+      isValid = false
+    }
+
+    setError(errorTemp)
+    return isValid
   }
 
   async function saveData() {
     try {
       // Desabilita o botão de enviar para evitar envios duplicados
       setSendBtnStatus({disabled: true, label: 'Enviando...'})
-
-      await axios.post('https://api.faustocintra.com.br/clientes', cliente)
-
+      
+      // Se estivermos editando, precisamos enviar os dados com o verbo HTTP PUT
+      if(params.id) await axios.put(`https://api.faustocintra.com.br/karangos/${params.id}`, karango)
+      // Senão, estaremos criando um novo registro, e o verbo HTTP a ser usado é o POST
+      else await axios.post('https://api.faustocintra.com.br/karangos', karango)
+      
       // Mostra a SnackBar
       setSbStatus({open: true, severity: 'success', message: 'Dados salvos com sucesso!'})
-
+      
     }
     catch(error) {
       // Mostra a SnackBar
@@ -155,7 +274,8 @@ export default function ClientesForm() {
 
     event.preventDefault()    // Evita que a página seja recarregada
 
-    saveData()
+    // Só envia para o banco de dados se o formulário for válido
+    if(validate(karango)) saveData()
 
   }
 
@@ -166,156 +286,155 @@ export default function ClientesForm() {
     if(sbStatus.severity === 'success') history.push('/list')
   }
 
+  function handleDialogClose(result) {
+    setDialogOpen(false)
+
+    // Se o usuário concordou em voltar 
+    if(result) history.push('/list')
+  }
+
+  function handleGoBack() {
+    // Se o formulário tiver sido modificado, exibimos o diálogo de confirmação
+    if(isModified) setDialogOpen(true)
+    // Senão, podemos voltar diretamente para a listagem
+    else history.push('/list')
+  }
+
   return (
     <>
+
+      <ConfirmDialog isOpen={dialogOpen} onClose={handleDialogClose}>
+        Há dados não salvos. Deseja realmente voltar?
+      </ConfirmDialog>
+
       <Snackbar open={sbStatus.open} autoHideDuration={6000} onClose={handleSbClose}>
         <MuiAlert elevation={6} variant="filled" onClose={handleSbClose} severity={sbStatus.severity}>
           {sbStatus.message}
         </MuiAlert>
       </Snackbar>
 
-      <h1>Cadastrar novo Cliente</h1>
+      <h1>{title}</h1>
       <form className={classes.form} onSubmit={handleSubmit}>
-
+        
         <TextField 
-          id="nome" 
-          label="Nome" 
+          id="marca" 
+          label="Marca" 
           variant="filled"
-          value={cliente.nome}
+          value={karango.marca}
           onChange={handleInputChange}
           required  /* not null, precisa ser preenchido */
-          placeholder="Informe o nome do cliente"
+          placeholder="Informe a marca do veículo"
           fullWidth
+          error={error.marca !== ''}
+          helperText={error.marca}
         />
+
+        <TextField 
+          id="modelo" 
+          label="Modelo" 
+          variant="filled"
+          value={karango.modelo}
+          onChange={handleInputChange}
+          required  /* not null, precisa ser preenchido */
+          placeholder="Informe o modelo do veículo"
+          fullWidth
+          error={error.modelo !== ''}
+          helperText={error.modelo}
+        />
+
+        <TextField 
+          id="cor" 
+          label="Cor" 
+          variant="filled"
+          value={karango.cor}
+          onChange={event => handleInputChange(event, 'cor')}
+          required  /* not null, precisa ser preenchido */
+          placeholder="Informe a cor do veículo"
+          select
+          fullWidth
+          error={error.cor !== ''}
+          helperText={error.cor}
+        >
+          { colors.map(color => <MenuItem value={color} key={color}>{color}</MenuItem>)}
+        </TextField>
+
+        <TextField 
+          id="ano_fabricacao" 
+          label="Ano de fabricação" 
+          variant="filled"
+          value={karango.ano_fabricacao}
+          onChange={event => handleInputChange(event, 'ano_fabricacao')}
+          required  /* not null, precisa ser preenchido */
+          placeholder="Informe o ano de fabricação do veículo"
+          select
+          fullWidth
+        >
+          { years.map(year => <MenuItem value={year} key={year}>{year}</MenuItem>)}
+        </TextField>
+
+        <FormControl fullWidth>
+          <FormControlLabel control={
+            <Checkbox
+              id="importado"
+              checked={importadoChecked}
+              onChange={handleInputChange}
+            />
+          }
+          label="Importado?"
+        />
+        </FormControl>
+
         <InputMask
-          id="cpf" 
-          mask={cpfMask}
+          id="placa" 
+          mask={placaMask}
           formatChars={formatChars}
-          value={cliente.cpfMask}
-          onChange={(event) => handleInputChange(event, 'cpf')}
+          value={karango.placa}
+          onChange={(event) => handleInputChange(event, 'placa')}
         >
           {() => <TextField 
-            label="CPF" 
+            label="Placa" 
             variant="filled"
             required  /* not null, precisa ser preenchido */
-            placeholder="Informe o CPF do Cliente"              fullWidth
+            placeholder="Informe a placa do veículo"
+            fullWidth
+            error={error.placa !== ''}
+            helperText={error.placa}
           />}
         </InputMask>
+
         <TextField 
-          id="rg" 
-          label="RG" 
+          id="preco" 
+          label="Preço" 
           variant="filled"
-          value={cliente.rg}
+          value={karango.preco}
           onChange={handleInputChange}
           required  /* not null, precisa ser preenchido */
-          placeholder="Informe o rg do cliente"
+          placeholder="Informe o valor do veículo"
           fullWidth
-        />
-        <TextField 
-          id="logradouro" 
-          label="Logradouro" 
-           variant="filled"
-          value={cliente.logradouro}
-          onChange={handleInputChange}
-          required  /* not null, precisa ser preenchido */
-          placeholder="Informe o logradouro"
-          select
-          fullWidth
+          type="number"
+          onFocus={event => event.target.select()}  // Seleciona o conteúdo ao focar
+          InputProps={{
+            startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+          }}
+          error={error.preco !== ''}
+          helperText={error.preco}
         />
 
-        <TextField 
-          id="num_imovel" 
-          label="Número" 
-          variant="filled"
-          value={cliente.num_imovel}
-          onChange={handleInputChange}
-          required  /* not null, precisa ser preenchido */
-          placeholder="Informe o número do imovel do cliente"
-          select
-          fullWidth
-        />
-          
-         <TextField 
-           id="complemento" 
-           label="Complemento" 
-           variant="filled"
-           value={cliente.complemento}
-           onChange={handleInputChange}
-           required  /* not null, precisa ser preenchido */
-           placeholder="Informe complemento do imovel do cliente"
-           fullWidth
-         />
-          <TextField 
-            id="bairro" 
-            label="Bairro" 
-            variant="filled"
-            value={cliente.bairro}
-            onChange={handleInputChange}
-            required  /* not null, precisa ser preenchido */
-            placeholder="Informe o bairro do imovel do cliente"
-            fullWidth
-          />
-          <TextField 
-            id="municipio" 
-            label="Município" 
-            variant="filled"
-            value={cliente.municipio}
-            onChange={handleInputChange}
-            required  /* not null, precisa ser preenchido */
-            placeholder="Informe o município do imovel do cliente"
-            fullWidth
-          />
+        <Toolbar className={classes.toolbar}>
+          <Button type="submit" variant="contained" color="secondary" disabled={sendBtnStatus.disabled}>
+            {sendBtnStatus.label}
+          </Button>
+          <Button variant="contained" onClick={handleGoBack}>Voltar</Button>
+        </Toolbar>
 
-        <InputMask
-            id="uf" 
-            mask={cpfMask}
-            formatChars={formatChars}
-            value={cliente.ufMask}
-            onChange={(event) => handleInputChange(event, 'uf')}
-          >
-            {() => <TextField 
-              label="UF" 
-              variant="filled"
-              required  /* not null, precisa ser preenchido */
-              placeholder="Informe o UF do imovel do cliente"
-              fullWidth
-            />}
-        </InputMask>
-
-          <TextField 
-            id="telefone" 
-            label="Telefone" 
-            variant="filled"
-            value={cliente.telefone}
-            onChange={handleInputChange}
-            required  /* not null, precisa ser preenchido */
-            placeholder="Informe o telefone do cliente"
-            fullWidth
-          />
-          <TextField 
-            id="telefone" 
-            label="Telefone" 
-            variant="filled"
-            value={cliente.telefone}
-            onChange={handleInputChange}
-            required  /* not null, precisa ser preenchido */
-            placeholder="Informe o telefone do cliente"
-            fullWidth
-          />
-  
-          <Toolbar className={classes.toolbar}>
-            <Button type="submit" variant="contained" color="secondary" disabled={sendBtnStatus.disabled}>
-              {sendBtnStatus.label}
-            </Button>
-            <Button variant="contained">Voltar</Button>
-          </Toolbar>
-  
-          <div>
-            {JSON.stringify(cliente)}
-            <br />
-            currentId: {JSON.stringify(currentId)}
-          </div>
-        </form>
-      </>
-    )
-  }
+        {/* <div>
+          {JSON.stringify(karango)}
+          <br />
+          currentId: {JSON.stringify(currentId)}
+          <br />
+          isModified: {JSON.stringify(isModified)}
+        </div> */}
+      </form>
+    </>
+  )
+}
